9142eea8b80aa65ae53fd96dd0559af1e2606213
René Muhl <rmuhl@exa-online.de>
2021-12-02 11:07:09 +0100

    Improve result puzzle screen
    - hide buttons and small badge
    - show achieved large badge

--- src/components/CompareQuantities.vue
+++ src/components/CompareQuantities.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="puzzle_body">
-    <img :src="currentPuzzleBody">
+    <img class="puzzle" :src="currentPuzzleBody">
+    <img class="puzzle_badge__large" :src="currentBadge" v-if="completed">
   </div>
-  <div class="puzzle_bottom">
+  <div class="puzzle_bottom" v-if="!completed">
     <div class="puzzle_buttons">
       <button v-for="(buttonImage, index) in buttonImages" :key="buttonImage" v-on:click="evalSelection(index)">
         <img :src="buttonImage">
       </button>
     </div>
-    <img class="puzzle_badge" :src="currentBadge">
+    <img class="puzzle_badge__small" :src="currentBadge">
   </div>
 </template>
 
@@ -17,6 +18,7 @@ export default {
   name: 'CompareQuantities',
   data() {
     return {
+      completed: false,
       badgeIndex: 0,
       puzzleIndex: 0,
       solutions: [0,2,1,0,1,0]
@@ -50,6 +52,9 @@ export default {
       if (this.puzzleIndex < this.solutions.length) {
         this.puzzleIndex++;
       }
+      if (this.puzzleIndex === this.solutions.length) {
+        this.completed = true;
+      }
     }
   }
 }
@@ -57,13 +62,19 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.puzzle_badge {
+.puzzle_badge__small {
   min-height: 7vh;
+}
+.puzzle_badge__large {
+  position: absolute;
+  top: 21%;
+  left: 50%;
+  z-index: 1000;
 }
 .puzzle_body {
   padding: 3vw;
 }
-.puzzle_body img {
+.puzzle_body .puzzle {
   min-height: 7vh;
   max-height: 45vh;
 }
