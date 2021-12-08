a32214d89e421d8520d6b3deb96477de8360f65f
René Muhl <rmuhl@exa-online.de>
2021-12-08 13:46:58 +0100

    Overall badge background

--- src/components/OverallBadge.vue
+++ src/components/OverallBadge.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="overall_badge">
+    <img class="overall_badge_background" :src="badgeBackground">
     <router-link class="add_quantities" to="/add_quantities" v-html="currentAddQuantitiesBadge" title="AddQuantities"></router-link>
     <router-link class="quantity_comparison" to="/quantity_comparison" v-html="currentQuantityComparisonBadge" title="QuantityComparison"></router-link>
   </div>
@@ -18,6 +19,9 @@ export default {
     currentAddQuantitiesBadge: function() {
       return this.currentBadgeByName('AddQuantities')
     },
+    badgeBackground: function() {
+      return require('../assets/overall_badge_background.svg')
+    }
   },
 };
 </script>
@@ -25,6 +29,19 @@ export default {
 #overall_badge {
   display: flex;
   justify-content: center;
+  align-items: center;
   gap: 2vw;
+  height: 100%;
+  width: 100%;
+  padding: 1vw 1vh;
+}
+.overall_badge_background {
+  z-index: -1000;
+}
+.add_quantities {
+  position: absolute;
+}
+.quantity_comparison {
+  position: absolute;
 }
 </style>
