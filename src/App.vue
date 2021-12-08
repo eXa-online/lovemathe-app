ab0050c8226928385637bd9e84f7575067880062
René Muhl <rmuhl@exa-online.de>
2021-12-08 10:50:24 +0100

    Add navigation to games

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/quantity_comparison">Quantity comparison</router-link> |
+    <router-link to="/add_quantities">Add Quantities</router-link>
+  </div>
   <router-view/>
 </template>
 
 <style>
 #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  color: #2c3e50;
 }
 body {
   background-image: url('assets/background.svg');
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
