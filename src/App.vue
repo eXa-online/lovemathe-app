8b4608b60cb6905e221c5437e5a4c375a46217c1
René Muhl <rmuhl@exa-online.de>
2022-01-09 10:30:10 +0100

    Remove unused styling

--- src/App.vue
+++ src/App.vue
@@ -22,17 +22,4 @@ body {
   margin: 0;
   height: 100%;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
