692d67ecff8a34a6b22d6b05ac4be285401b9ca1
Leonard Speer <lspeer@exa-online.de>
2022-02-10 09:30:40 +0100

    Adapt Quantity Equality to new design

--- src/components/QuantityEquality.vue
+++ src/components/QuantityEquality.vue
@@ -2,7 +2,7 @@
   <GameTemplate
     gameName="QuantityEquality"
     gamePath="quantity_equality"
-    title="Siehst du genau so viele Sterne wie Kreise? Wenn ja, klicke auf das lachende und wenn nicht, auf das traurige Gesicht."
+    title="Siehst du genau so viele Brillen wie Hüte? Wenn ja, klicke auf das lachende und wenn nicht, auf das traurige Gesicht."
     :countButtons="2"
     :showDemo="true"
     :solutions="[1,0,1,1,0]"/>
