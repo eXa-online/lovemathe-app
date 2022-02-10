9a6f98f6cfc942c5f809055da64e6a71b11ee08a
Leonard Speer <lspeer@exa-online.de>
2022-02-10 09:19:48 +0100

    Fix Reduce Quantities title

--- src/components/ReduceQuantities.vue
+++ src/components/ReduceQuantities.vue
@@ -2,7 +2,7 @@
   <GameTemplate
     gameName="ReduceQuantities"
     gamePath="reduce_quantities"
-    title="Ich brauche genau so viele Kreise wie Sterne. Was ist zu viel?"
+    title="Ich brauche genau so viele Tomaten wie Schaufeln. Was ist zu viel?"
     :countButtons="3"
     :showDemo="true"
     :solutions="[2,1,0,1,2,2]"/>
