7a93c9285d86b762a11c90a88ed5df91edf9363d
René Muhl <rmuhl@exa-online.de>
2022-02-10 15:30:36 +0100

    Rename 'Counting One' to 'Counting'

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AddQuantities from '@/components/AddQuantities.vue'
-import CountingOne from '@/components/CountingOne.vue'
+import Counting from '@/components/Counting.vue'
 import OneLook from '@/components/OneLook.vue'
 import OverallBadge from '@/components/OverallBadge.vue'
 import QuantityComparison from '@/components/QuantityComparison.vue'
@@ -10,7 +10,7 @@ import ReduceQuantities from '@/components/ReduceQuantities.vue'
 const routes = [
   { path: '/', component: OverallBadge },
   { path: '/add_quantities', component: AddQuantities },
-  { path: '/counting_one', component: CountingOne },
+  { path: '/counting', component: Counting },
   { path: '/one_look', component: OneLook },
   { path: '/quantity_comparison', component: QuantityComparison },
   { path: '/quantity_equality', component: QuantityEquality },
