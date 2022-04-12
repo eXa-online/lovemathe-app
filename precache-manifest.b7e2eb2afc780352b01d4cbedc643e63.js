c1d889fd48ee1a0424156717ebc1b2be93cc2a0e
CSchmidtDev <>
2022-04-12 11:38:41 +0000

    Deploying to gh-pages from @ eXa-online/lovemathe@03983cf7bdcb732bcd69680fb193425a7124a3a6 🚀

---
+++ precache-manifest.b7e2eb2afc780352b01d4cbedc643e63.js
@@ -0,0 +1,630 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87fff59c760c77ba20dc0be6083cd238",
+    "url": "/lovemathe/404.html"
+  },
+  {
+    "revision": "7808d2197848e0cc55ba",
+    "url": "/lovemathe/css/app.7c7a8ee9.css"
+  },
+  {
+    "revision": "088b0783205fe47e058f1f900426afe8",
+    "url": "/lovemathe/img/0.088b0783.svg"
+  },
+  {
+    "revision": "10ea12562dd427ded50bfb33ce65fa8e",
+    "url": "/lovemathe/img/0.10ea1256.svg"
+  },
+  {
+    "revision": "22dcc7a802c9cc9c9131f6d3cab3c905",
+    "url": "/lovemathe/img/0.22dcc7a8.svg"
+  },
+  {
+    "revision": "26310ed691f28db7de0cb0623ecb0cd4",
+    "url": "/lovemathe/img/0.26310ed6.svg"
+  },
+  {
+    "revision": "2f882e62da1da6678a81f59a54cf4854",
+    "url": "/lovemathe/img/0.2f882e62.svg"
+  },
+  {
+    "revision": "35f0e25c0601eb8752fa9ab30752fd04",
+    "url": "/lovemathe/img/0.35f0e25c.svg"
+  },
+  {
+    "revision": "6a111b31b5acddc08ae8602648cb2a16",
+    "url": "/lovemathe/img/0.6a111b31.svg"
+  },
+  {
+    "revision": "6d61fbc5ac0b57fd591850b0332052f3",
+    "url": "/lovemathe/img/0.6d61fbc5.svg"
+  },
+  {
+    "revision": "70a4b012f76bf8fe998dc841f722ca8e",
+    "url": "/lovemathe/img/0.70a4b012.svg"
+  },
+  {
+    "revision": "747c052e6bfaccfab1655b9ab09c1916",
+    "url": "/lovemathe/img/0.747c052e.svg"
+  },
+  {
+    "revision": "7559abf56b6483546c6251e4a80a9161",
+    "url": "/lovemathe/img/0.7559abf5.svg"
+  },
+  {
+    "revision": "8b39779cb36ad2a2842d8a07a1e32ba6",
+    "url": "/lovemathe/img/0.8b39779c.svg"
+  },
+  {
+    "revision": "921bf8d08ae0e8076cb20dbc90747481",
+    "url": "/lovemathe/img/0.921bf8d0.svg"
+  },
+  {
+    "revision": "96605d67016dfaf124a4d8db411feeac",
+    "url": "/lovemathe/img/0.96605d67.svg"
+  },
+  {
+    "revision": "9d35e73d5edc086767398d65ba9da6c8",
+    "url": "/lovemathe/img/0.9d35e73d.svg"
+  },
+  {
+    "revision": "ad1a137d2c3990204e87589d588664dc",
+    "url": "/lovemathe/img/0.ad1a137d.svg"
+  },
+  {
+    "revision": "b328e23f8e0a37c3cd8e13b816bfae0e",
+    "url": "/lovemathe/img/0.b328e23f.svg"
+  },
+  {
+    "revision": "b50f98640c3d45b5a9cafee8566c85d5",
+    "url": "/lovemathe/img/0.b50f9864.svg"
+  },
+  {
+    "revision": "baa44978ee3fd76a7a8028926e9caf40",
+    "url": "/lovemathe/img/0.baa44978.svg"
+  },
+  {
+    "revision": "d5aef3a9689031a973971255140daf0c",
+    "url": "/lovemathe/img/0.d5aef3a9.svg"
+  },
+  {
+    "revision": "eac9eab6cc2c83f9e459dbff1f525fe5",
+    "url": "/lovemathe/img/0.eac9eab6.svg"
+  },
+  {
+    "revision": "f57c9706ff052dbdee528dcb89631632",
+    "url": "/lovemathe/img/0.f57c9706.svg"
+  },
+  {
+    "revision": "f5e74f763a8ee28beba5b35f24abfea8",
+    "url": "/lovemathe/img/0.f5e74f76.svg"
+  },
+  {
+    "revision": "0293903e5f79d1108a6963b34ac393c3",
+    "url": "/lovemathe/img/1.0293903e.svg"
+  },
+  {
+    "revision": "03c84fcb8c1ff579ced8b95028db2c16",
+    "url": "/lovemathe/img/1.03c84fcb.svg"
+  },
+  {
+    "revision": "05010b4804541f93f05005376dc485d6",
+    "url": "/lovemathe/img/1.05010b48.svg"
+  },
+  {
+    "revision": "0e25615ddd97cea83e87b29ef4b86fcb",
+    "url": "/lovemathe/img/1.0e25615d.svg"
+  },
+  {
+    "revision": "10aaaf0914566cb4b1a4afad723c5816",
+    "url": "/lovemathe/img/1.10aaaf09.svg"
+  },
+  {
+    "revision": "18aafdaf609121824cc110c9f66a4f07",
+    "url": "/lovemathe/img/1.18aafdaf.svg"
+  },
+  {
+    "revision": "27853be3af6e151b1e150f02866dea18",
+    "url": "/lovemathe/img/1.27853be3.svg"
+  },
+  {
+    "revision": "3376f3bd3ae54c63b9824d231731bf31",
+    "url": "/lovemathe/img/1.3376f3bd.svg"
+  },
+  {
+    "revision": "551d493008338cf3292ee87a5a561b19",
+    "url": "/lovemathe/img/1.551d4930.svg"
+  },
+  {
+    "revision": "64f63067840741d14867a9862c3f8911",
+    "url": "/lovemathe/img/1.64f63067.svg"
+  },
+  {
+    "revision": "6c54220becca95b9576e85e01a146672",
+    "url": "/lovemathe/img/1.6c54220b.svg"
+  },
+  {
+    "revision": "6ccc61e2162beb17dbec25ea560e2afe",
+    "url": "/lovemathe/img/1.6ccc61e2.svg"
+  },
+  {
+    "revision": "772098174826cdc2e8fb055939235c6d",
+    "url": "/lovemathe/img/1.77209817.svg"
+  },
+  {
+    "revision": "7b0d5930b3538eab53768b493d9d0054",
+    "url": "/lovemathe/img/1.7b0d5930.svg"
+  },
+  {
+    "revision": "7b68b0e4d144bfebf52e8beef51a7346",
+    "url": "/lovemathe/img/1.7b68b0e4.svg"
+  },
+  {
+    "revision": "af8af555693d490080ce32adc3c1d6e9",
+    "url": "/lovemathe/img/1.af8af555.svg"
+  },
+  {
+    "revision": "c56104450d4250dabfc9353a528855e8",
+    "url": "/lovemathe/img/1.c5610445.svg"
+  },
+  {
+    "revision": "ca438b47823e17d6d48d763f7a38da9a",
+    "url": "/lovemathe/img/1.ca438b47.svg"
+  },
+  {
+    "revision": "d983ca3cb66341cc31758208de61bf9a",
+    "url": "/lovemathe/img/1.d983ca3c.svg"
+  },
+  {
+    "revision": "e4544093cfe2a52dcc685af3bb336d41",
+    "url": "/lovemathe/img/1.e4544093.svg"
+  },
+  {
+    "revision": "f19d91b38ec9f55161a4dcf0e15c225b",
+    "url": "/lovemathe/img/1.f19d91b3.svg"
+  },
+  {
+    "revision": "f8530c2b0927d51183f2c4aad3f2920b",
+    "url": "/lovemathe/img/1.f8530c2b.svg"
+  },
+  {
+    "revision": "f92a720ee59e7729db37583e4aeb7a41",
+    "url": "/lovemathe/img/1.f92a720e.svg"
+  },
+  {
+    "revision": "00ab99020e200d1e6d08cfef2d5d77f3",
+    "url": "/lovemathe/img/2.00ab9902.svg"
+  },
+  {
+    "revision": "0c6dfa8d53d458b92f4fb1dac3645db2",
+    "url": "/lovemathe/img/2.0c6dfa8d.svg"
+  },
+  {
+    "revision": "441d289c4920de74ea544c6149dd3243",
+    "url": "/lovemathe/img/2.441d289c.svg"
+  },
+  {
+    "revision": "5e502191a280b7fb0a981f0371ae8f49",
+    "url": "/lovemathe/img/2.5e502191.svg"
+  },
+  {
+    "revision": "6d34ee8a0b3928951fe59864ea950b0c",
+    "url": "/lovemathe/img/2.6d34ee8a.svg"
+  },
+  {
+    "revision": "6fc58c552ecd70422f5152142731bfec",
+    "url": "/lovemathe/img/2.6fc58c55.svg"
+  },
+  {
+    "revision": "7d2aa7bcf8641ce623beaf6fce0a9769",
+    "url": "/lovemathe/img/2.7d2aa7bc.svg"
+  },
+  {
+    "revision": "7db72dc72836910b292ab4f5586da864",
+    "url": "/lovemathe/img/2.7db72dc7.svg"
+  },
+  {
+    "revision": "933a5debb439bb568e058e5d93c67caa",
+    "url": "/lovemathe/img/2.933a5deb.svg"
+  },
+  {
+    "revision": "961a5b4f7d37a58117868cf8c8353c64",
+    "url": "/lovemathe/img/2.961a5b4f.svg"
+  },
+  {
+    "revision": "98234eb521a58b9fa6da98f63b1d9ac1",
+    "url": "/lovemathe/img/2.98234eb5.svg"
+  },
+  {
+    "revision": "9d6c2e01d43bfcd57885420c6866bae6",
+    "url": "/lovemathe/img/2.9d6c2e01.svg"
+  },
+  {
+    "revision": "a2a7a6b269efdd3931c85168dfe68052",
+    "url": "/lovemathe/img/2.a2a7a6b2.svg"
+  },
+  {
+    "revision": "a5a2edcf353b1de80ec8cb774d556842",
+    "url": "/lovemathe/img/2.a5a2edcf.svg"
+  },
+  {
+    "revision": "ad15fc6bac0ce0a8d93982783ae02db2",
+    "url": "/lovemathe/img/2.ad15fc6b.svg"
+  },
+  {
+    "revision": "b679d0d26866d1551f36724298aee2fc",
+    "url": "/lovemathe/img/2.b679d0d2.svg"
+  },
+  {
+    "revision": "d692d1a87a1226bafd683015791d8fd0",
+    "url": "/lovemathe/img/2.d692d1a8.svg"
+  },
+  {
+    "revision": "e65611388450f150232fe5901b72303f",
+    "url": "/lovemathe/img/2.e6561138.svg"
+  },
+  {
+    "revision": "e8db00de482d200e682984fc13897bfc",
+    "url": "/lovemathe/img/2.e8db00de.svg"
+  },
+  {
+    "revision": "f50194aaa7da90f14a76e31e011df050",
+    "url": "/lovemathe/img/2.f50194aa.svg"
+  },
+  {
+    "revision": "f68561f582adf5bc859bac5d2e2a2a95",
+    "url": "/lovemathe/img/2.f68561f5.svg"
+  },
+  {
+    "revision": "fb141c3be18ee9669fff8e10c4f74087",
+    "url": "/lovemathe/img/2.fb141c3b.svg"
+  },
+  {
+    "revision": "061a3850ab942f60f2c341633bed193f",
+    "url": "/lovemathe/img/3.061a3850.svg"
+  },
+  {
+    "revision": "1faddab01f0a22fa81d54cedf746003e",
+    "url": "/lovemathe/img/3.1faddab0.svg"
+  },
+  {
+    "revision": "24854d4989af8fbf213ce0100e4636ad",
+    "url": "/lovemathe/img/3.24854d49.svg"
+  },
+  {
+    "revision": "2698e98f84199d67edd3411378b937dc",
+    "url": "/lovemathe/img/3.2698e98f.svg"
+  },
+  {
+    "revision": "3960b5a752b41eccdde2537ab7c79575",
+    "url": "/lovemathe/img/3.3960b5a7.svg"
+  },
+  {
+    "revision": "49051dd85e7d848fa51434b0d8093422",
+    "url": "/lovemathe/img/3.49051dd8.svg"
+  },
+  {
+    "revision": "498a35a3951262c3b9b4e2a383a89754",
+    "url": "/lovemathe/img/3.498a35a3.svg"
+  },
+  {
+    "revision": "58ff0ea47df1d1f3373c6eae4a872a38",
+    "url": "/lovemathe/img/3.58ff0ea4.svg"
+  },
+  {
+    "revision": "6340770ac72b63d8a57e1b10274f5bec",
+    "url": "/lovemathe/img/3.6340770a.svg"
+  },
+  {
+    "revision": "664b7b6dd4f513dbedb57b57317f1d9f",
+    "url": "/lovemathe/img/3.664b7b6d.svg"
+  },
+  {
+    "revision": "7b646a1789a776a5c17e62776c1a4ad6",
+    "url": "/lovemathe/img/3.7b646a17.svg"
+  },
+  {
+    "revision": "7c4dfb8c5e3093185f85d502c353f25f",
+    "url": "/lovemathe/img/3.7c4dfb8c.svg"
+  },
+  {
+    "revision": "8ac7c0d7824ffe619ca2a9054935ff1e",
+    "url": "/lovemathe/img/3.8ac7c0d7.svg"
+  },
+  {
+    "revision": "8f1c3f4b77e121deae0a4ce7b77d5537",
+    "url": "/lovemathe/img/3.8f1c3f4b.svg"
+  },
+  {
+    "revision": "f23ec740f68edb7b8192b4f9e4630637",
+    "url": "/lovemathe/img/3.f23ec740.svg"
+  },
+  {
+    "revision": "f402b0f77c8d412e8cc33aaa6ce26574",
+    "url": "/lovemathe/img/3.f402b0f7.svg"
+  },
+  {
+    "revision": "f5e266879aee38952bcc63655f9b3678",
+    "url": "/lovemathe/img/3.f5e26687.svg"
+  },
+  {
+    "revision": "fcffb45778ff574f228693db30cf3342",
+    "url": "/lovemathe/img/3.fcffb457.svg"
+  },
+  {
+    "revision": "0ac48ef5f6908657eb5fec71e1ccb0a4",
+    "url": "/lovemathe/img/4.0ac48ef5.svg"
+  },
+  {
+    "revision": "1195414e65eb9f0b88a4d1a67ddf9291",
+    "url": "/lovemathe/img/4.1195414e.svg"
+  },
+  {
+    "revision": "1aa6d5edb690b8473a2057f724a97155",
+    "url": "/lovemathe/img/4.1aa6d5ed.svg"
+  },
+  {
+    "revision": "48804cb8907c5ac98ff5b32468059022",
+    "url": "/lovemathe/img/4.48804cb8.svg"
+  },
+  {
+    "revision": "5361f4aa3a93a7f639cc6c3ba6d49e3c",
+    "url": "/lovemathe/img/4.5361f4aa.svg"
+  },
+  {
+    "revision": "672cfb7bf6a39cdaa0c8d16d773cc3e0",
+    "url": "/lovemathe/img/4.672cfb7b.svg"
+  },
+  {
+    "revision": "68b6d1c6d0a9e1c8258b85bc270c94c0",
+    "url": "/lovemathe/img/4.68b6d1c6.svg"
+  },
+  {
+    "revision": "68df2d4d62f5b3d4bc91a64aa9bba4fb",
+    "url": "/lovemathe/img/4.68df2d4d.svg"
+  },
+  {
+    "revision": "8d2ab861284417745fbb3b07dbd13ead",
+    "url": "/lovemathe/img/4.8d2ab861.svg"
+  },
+  {
+    "revision": "a3a78e2c14743b5b2a2517499653629f",
+    "url": "/lovemathe/img/4.a3a78e2c.svg"
+  },
+  {
+    "revision": "a8e0072b9af10bf206ec63cdbb13862b",
+    "url": "/lovemathe/img/4.a8e0072b.svg"
+  },
+  {
+    "revision": "a9ce09a8068733a283f49199ed22ee17",
+    "url": "/lovemathe/img/4.a9ce09a8.svg"
+  },
+  {
+    "revision": "c00361ea9f0c3c53eea9ac5c4fe0d719",
+    "url": "/lovemathe/img/4.c00361ea.svg"
+  },
+  {
+    "revision": "c0a3ce86bb361b8448226f2056738762",
+    "url": "/lovemathe/img/4.c0a3ce86.svg"
+  },
+  {
+    "revision": "e4eb957a88c61748a6f6b972fb9d68cd",
+    "url": "/lovemathe/img/4.e4eb957a.svg"
+  },
+  {
+    "revision": "f518c44d2c800340f4b38f73970850ca",
+    "url": "/lovemathe/img/4.f518c44d.svg"
+  },
+  {
+    "revision": "f7accffd62809409d4bece48112e0fd2",
+    "url": "/lovemathe/img/4.f7accffd.svg"
+  },
+  {
+    "revision": "f8ecaf34ffaf6a90718f320629acbb8e",
+    "url": "/lovemathe/img/4.f8ecaf34.svg"
+  },
+  {
+    "revision": "34d32fdee5255093618b59014777e3f6",
+    "url": "/lovemathe/img/5.34d32fde.svg"
+  },
+  {
+    "revision": "4b0cbf60ad1a9274eb7ba3481747c5f0",
+    "url": "/lovemathe/img/5.4b0cbf60.svg"
+  },
+  {
+    "revision": "5af7034d67ce2e44686a00be2dbcc6e0",
+    "url": "/lovemathe/img/5.5af7034d.svg"
+  },
+  {
+    "revision": "653d0cf1034176f40d0b564a498e3766",
+    "url": "/lovemathe/img/5.653d0cf1.svg"
+  },
+  {
+    "revision": "667498c8f2cc1c95bef591fd0d22c1f9",
+    "url": "/lovemathe/img/5.667498c8.svg"
+  },
+  {
+    "revision": "66d1f641d135772fdbc2c9d02e476c1c",
+    "url": "/lovemathe/img/5.66d1f641.svg"
+  },
+  {
+    "revision": "76bb0d8a97223554f4b1479b6d53400f",
+    "url": "/lovemathe/img/5.76bb0d8a.svg"
+  },
+  {
+    "revision": "81936e8b941c24953d48188a6dde7c65",
+    "url": "/lovemathe/img/5.81936e8b.svg"
+  },
+  {
+    "revision": "87b56344a96d50701fec4234daff818f",
+    "url": "/lovemathe/img/5.87b56344.svg"
+  },
+  {
+    "revision": "94f7e79195e4a88201338be45e5fa627",
+    "url": "/lovemathe/img/5.94f7e791.svg"
+  },
+  {
+    "revision": "ada918c04a77feefb20bdfb90543ec98",
+    "url": "/lovemathe/img/5.ada918c0.svg"
+  },
+  {
+    "revision": "bacf0d90056a5522b3742e7e2ebb0d41",
+    "url": "/lovemathe/img/5.bacf0d90.svg"
+  },
+  {
+    "revision": "bbe0c76b38c916b032ba93b710e57eb9",
+    "url": "/lovemathe/img/5.bbe0c76b.svg"
+  },
+  {
+    "revision": "c73872defc49077fdbbd9373ff322c46",
+    "url": "/lovemathe/img/5.c73872de.svg"
+  },
+  {
+    "revision": "cdf1d2c19fa8fc6333be840b798d65b6",
+    "url": "/lovemathe/img/5.cdf1d2c1.svg"
+  },
+  {
+    "revision": "d1ed804577b831c6c647d3bcdfea15f9",
+    "url": "/lovemathe/img/5.d1ed8045.svg"
+  },
+  {
+    "revision": "d412f48490c53645e4424f51e271e60d",
+    "url": "/lovemathe/img/5.d412f484.svg"
+  },
+  {
+    "revision": "fe09dff80967694e811629d2f5e1a1a0",
+    "url": "/lovemathe/img/5.fe09dff8.svg"
+  },
+  {
+    "revision": "38f30f99deb373fcae80663246306e7f",
+    "url": "/lovemathe/img/6.38f30f99.svg"
+  },
+  {
+    "revision": "5855c5b60cc6c1193f6371456391b9a9",
+    "url": "/lovemathe/img/6.5855c5b6.svg"
+  },
+  {
+    "revision": "667498c8f2cc1c95bef591fd0d22c1f9",
+    "url": "/lovemathe/img/6.667498c8.svg"
+  },
+  {
+    "revision": "766dbcdd764bb9b62d9c2828a888a320",
+    "url": "/lovemathe/img/6.766dbcdd.svg"
+  },
+  {
+    "revision": "817a1d06e64c379f4c2ef1ef4b4b3da0",
+    "url": "/lovemathe/img/6.817a1d06.svg"
+  },
+  {
+    "revision": "a662bc899f12e79d04b357530c852cc3",
+    "url": "/lovemathe/img/6.a662bc89.svg"
+  },
+  {
+    "revision": "ca48485f03d4ce6a8ee1c6e981b00fb0",
+    "url": "/lovemathe/img/6.ca48485f.svg"
+  },
+  {
+    "revision": "dcbac699dbecbd7e25d99446d81cce51",
+    "url": "/lovemathe/img/6.dcbac699.svg"
+  },
+  {
+    "revision": "de4597b1383f1b99db0ebff0a1f6e28f",
+    "url": "/lovemathe/img/6.de4597b1.svg"
+  },
+  {
+    "revision": "c6fea0afe9f60b424f4e5b9205749c7d",
+    "url": "/lovemathe/img/badge_background.c6fea0af.svg"
+  },
+  {
+    "revision": "812761d18bff3007148bce6937101483",
+    "url": "/lovemathe/img/help.812761d1.svg"
+  },
+  {
+    "revision": "4be6eaecf4fe6ba93723dcc7e47ca1d0",
+    "url": "/lovemathe/img/kid_has_no_problems.4be6eaec.svg"
+  },
+  {
+    "revision": "3ed1d40a3d5472f3d17e4a09275f9870",
+    "url": "/lovemathe/img/overall_badge_background.3ed1d40a.svg"
+  },
+  {
+    "revision": "e3b626283c2c6ad13017a385e5a1bb32",
+    "url": "/lovemathe/img/overall_badge_background_color.e3b62628.svg"
+  },
+  {
+    "revision": "7afbe4bef33b27de21d6d734b5da92a6",
+    "url": "/lovemathe/index.html"
+  },
+  {
+    "revision": "7808d2197848e0cc55ba",
+    "url": "/lovemathe/js/app.b397d43f.js"
+  },
+  {
+    "revision": "43b76af6af91e8d5ff08",
+    "url": "/lovemathe/js/chunk-vendors.5151951c.js"
+  },
+  {
+    "revision": "cfa184234af632040f5959b9c74af26e",
+    "url": "/lovemathe/manifest.json"
+  },
+  {
+    "revision": "aa06e8d334f2a80e4a98a2f0f0ca5964",
+    "url": "/lovemathe/media/0.aa06e8d3.mp3"
+  },
+  {
+    "revision": "926f7a8491875b073e54293d961deeb6",
+    "url": "/lovemathe/media/1.926f7a84.mp3"
+  },
+  {
+    "revision": "c15534d5adab2abf6a1ce9892417259d",
+    "url": "/lovemathe/media/2.c15534d5.mp3"
+  },
+  {
+    "revision": "56bd667b2b8ab5d276570e401303388d",
+    "url": "/lovemathe/media/3.56bd667b.mp3"
+  },
+  {
+    "revision": "2523c838ad3989fbe8f800b07ea35ba5",
+    "url": "/lovemathe/media/4.2523c838.mp3"
+  },
+  {
+    "revision": "8b1a0d6095ebf44b11ba4bbd19474b3d",
+    "url": "/lovemathe/media/5.8b1a0d60.mp3"
+  },
+  {
+    "revision": "0b765fd45a2699b18a02df44cd832e84",
+    "url": "/lovemathe/media/instruction.0b765fd4.mp3"
+  },
+  {
+    "revision": "18d283912bdab5cd65c5cdd035905ec8",
+    "url": "/lovemathe/media/instruction.18d28391.mp3"
+  },
+  {
+    "revision": "9b482037234a9c701f6ff65f5a1c487f",
+    "url": "/lovemathe/media/instruction.9b482037.mp3"
+  },
+  {
+    "revision": "c7c214a81898298cba014a96c0e8943f",
+    "url": "/lovemathe/media/instruction.c7c214a8.mp3"
+  },
+  {
+    "revision": "d9b88a9a7b9b9c9dc15ee0774d338d5d",
+    "url": "/lovemathe/media/instruction.d9b88a9a.mp3"
+  },
+  {
+    "revision": "dcc568d8e2d9ab7b3cac975616f54689",
+    "url": "/lovemathe/media/instruction.dcc568d8.mp3"
+  },
+  {
+    "revision": "f951b6c492e509a5667b0d843e5ebbde",
+    "url": "/lovemathe/media/instruction.f951b6c4.mp3"
+  },
+  {
+    "revision": "ff71b8ce4730815319741748b4631867",
+    "url": "/lovemathe/media/instruction.ff71b8ce.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/lovemathe/robots.txt"
+  }
+]);
