aa350edb7540b2858ddb76b71d2ac64ed1cc8b4e
Chris Schmidt <cschmidt@exa-online.de>
2022-04-19 09:38:25 +0200

    create 2 games for tutoring

---
+++ src/components/tutoring/TutoringOverallBadge.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <router-link class="" to="/tutoring/vorschule/simultanerfassung_one" title="Level 1">
+      Simultanerfassung LVL-1
+    </router-link>
+    <br>
+    <router-link class="" to="/tutoring/vorschule/simultanerfassung_two" title="Level 1" v-bind:class="[simultanerfassungDone.includes(1) ? 'gameEnabled' :  'gameDisabled']">
+      Simultanerfassung LVL-2
+    </router-link>
+  </div>
+</template>
+
+<script>
+import {mapGetters, mapState} from 'vuex'
+
+export default {
+  name: "TutoringOverallBadge",
+  computed: {
+    ...mapGetters(['currentBadgeByName','allGamesDone']),
+    ...mapState(['completedGames', 'badgeIndexes', 'isFine']),
+    ...mapState({
+      simultanerfassungDone: state => state.simultanerfassung.completedGames
+    })
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,10 @@ import ReduceQuantities from '@/components/ReduceQuantities.vue'
 import Seriation from '@/components/Seriation.vue'
 import Orientation from '@/components/Orientation.vue'
 
+import TutoringOverallBadge from '@/components/tutoring/TutoringOverallBadge.vue'
+import SimultanerfassungOne from '@/components/tutoring/SimultanerfassungOne';
+import SimultanerfassungTwo from '@/components/tutoring/SimultanerfassungTwo';
+
 const routes = [
   { path: '/', component: OverallBadge },
   { path: '/add_quantities', component: AddQuantities },
@@ -19,6 +23,10 @@ const routes = [
   { path: '/reduce_quantities', component: ReduceQuantities },
   { path: '/seriation', component: Seriation },
   { path: '/orientation', component: Orientation },
+
+  { path: '/tutoring/vorschule', component: TutoringOverallBadge },
+  { path: '/tutoring/vorschule/simultanerfassung_one', component: SimultanerfassungOne },
+  { path: '/tutoring/vorschule/simultanerfassung_two', component: SimultanerfassungTwo },
 ]
 
 const router = createRouter({
