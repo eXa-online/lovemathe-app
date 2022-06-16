e826d349e432cbbfc50befceed30290f5c238b2c
CSchmidtDev <>
2022-06-16 16:00:09 +0000

    Deploying to gh-pages from @ eXa-online/lovemathe@e12db155d40afbfa563c06637e037382b122bedc 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lovemathe/precache-manifest.ab7aa6fbf81966ac7b4b490f2d4a7a57.js"
+  "/lovemathe/precache-manifest.5b38641879858f03e6e3094f855b7e6e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "lovemathe"});
