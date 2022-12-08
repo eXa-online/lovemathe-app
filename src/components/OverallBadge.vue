072e387437880aa84609e32ece07fe226f43310d
René Muhl <rmuhl@exa-online.de>
2022-12-08 10:56:18 +0100

    Remove unused second grid

--- src/components/OverallBadge.vue
+++ src/components/OverallBadge.vue
@@ -143,9 +143,6 @@ export default {
   display: grid;
   align-items: start;
   justify-items: start;
-  grid-template-columns: repeat(20, minmax(16px, 100%));
-  grid-template-rows: repeat(20, minmax(16px, 100%));
-  grid-gap: 0;
   margin: 0;
   padding: 0;
   width: 100%;
