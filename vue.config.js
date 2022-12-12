cbaf50839a205efa56b308a43710438e83baa717
René Muhl <rmuhl@exa-online.de>
2022-12-12 09:20:26 +0100

    LM-91 Show update information

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/lovemathe/' : '/',
+  pwa: {
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: './src/sw.js',
+      swDest: 'service-worker.js',
+    },
+  },
 }
