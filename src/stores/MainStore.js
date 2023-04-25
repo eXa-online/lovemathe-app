963b72ffb1a4c046b23f9c1b3f9bd75ef405b9cf
Daniel Weiß <dweiss@exa-online.de>
2023-04-25 10:18:47 +0200

    LM-123 Add posibility in the state to reset it to initial values

--- src/stores/MainStore.js
+++ src/stores/MainStore.js
@@ -1,57 +1,76 @@
 import { defineStore } from 'pinia'
 
+const initialState = {
+  completedGames: new Set(),
+  gameOrder: ['Count_Up', 'Quantity_Equality', 'Add_Quantities', 'Quantity_Comparison', 'Reduce_Quantities', 'One_Look', 'Complete_Seriation', 'Where_Is'],
+  evalGameOrder: ['Count_Up', 'Quantity_Equality', 'Quantities', 'One_Look', 'Complete_Seriation', 'Where_Is'],
+  nextGame: 'Count_Up',
+  gameInfos: {
+    'Quantities': {
+      "type": "group",
+      "verboseName": 'Mengen',
+      "subGames": ['Add_Quantities', 'Quantity_Comparison', 'Reduce_Quantities'],
+    },
+    'Quantity_Equality': {
+      "type": "game",
+      "verboseName": 'Mengengleichheit',
+      "level": 0,
+      "maxLevel": 6,
+    },
+    'Quantity_Comparison': {
+      "type": "game",
+      "verboseName": 'Mengenvergleich',
+      "level": 0,
+      "maxLevel": 6,
+    },
+    'Add_Quantities': {
+      "type": "game",
+      "verboseName": 'Mengen ergänzen',
+      "level": 0,
+      "maxLevel": 6,
+    },
+    'Reduce_Quantities': {
+      "type": "game",
+      "verboseName": 'Mengen reduzieren',
+      "level": 0,
+      "maxLevel": 6,
+    },
+    'Count_Up': {
+      "type": "game",
+      "verboseName": 'Abzählen',
+      "level": 0,
+      "maxLevel": 6,
+    },
+    'One_Look': {
+      "type": "game",
+      "verboseName": 'Simultanerfassung',
+      "level": 0,
+      "maxLevel": 6,
+    },
+    'Complete_Seriation': {
+      "type": "game",
+      "verboseName": 'Seriation',
+      "level": 0,
+      "maxLevel": 6,
+    },
+    'Where_Is': {
+      "type": "game",
+      "verboseName": 'Orientierung',
+      "level": 0,
+      "maxLevel": 6,
+    },
+  }
+}
+
 export const useMainStore = defineStore('MainStore', {
-  state: () => ({
-    completedGames: new Set(),
-    gameOrder: ['Count_Up', 'Quantity_Equality', 'Add_Quantities', 'Quantity_Comparison', 'Reduce_Quantities', 'One_Look', 'Complete_Seriation', 'Where_Is'],
-    nextGame: 'Count_Up',
-    gameInfos: {
-      'Quantity_Equality': {
-        "verboseName": 'Mengengleichheit',
-        "level": 0,
-        "maxLevel": 6,
-      },
-      'Quantity_Comparison': {
-        "verboseName": 'Mengenvergleich',
-        "level": 0,
-        "maxLevel": 6,
-      },
-      'Add_Quantities': {
-        "verboseName": 'Mengen ergänzen',
-        "level": 0,
-        "maxLevel": 6,
-      },
-      'Reduce_Quantities': {
-        "verboseName": 'Mengen reduzieren',
-        "level": 0,
-        "maxLevel": 6,
-      },
-      'Count_Up': {
-        "verboseName": 'Abzählen',
-        "level": 0,
-        "maxLevel": 6,
-      },
-      'One_Look': {
-        "verboseName": 'Simultanerfassung',
-        "level": 0,
-        "maxLevel": 6,
-      },
-      'Complete_Seriation': {
-        "verboseName": 'Seriation',
-        "level": 0,
-        "maxLevel": 6,
-      },
-      'Where_Is': {
-        "verboseName": 'Orientierung',
-        "level": 0,
-        "maxLevel": 6,
-      },
-    }
-  }),
+  state: () => ({...initialState}),
   getters: {
     currentBadgeByName: (state) => (name) => {
       const level = state.gameInfos[name]['level'];
       return require(`../assets/${name.toLowerCase()}/badges/${level}.svg`);
+    },
+    getDataByName: (state) => (name) => {
+      return state.gameInfos[name];
     },
     verboseNameByName: (state) => (name) => {
       return state.gameInfos[name]['verboseName'];
@@ -78,6 +97,9 @@ export const useMainStore = defineStore('MainStore', {
     }
   },
   actions: {
+    fullReset() {
+      this.state = initialState
+    },
     postGameSetup(payload) {
       this.completeGame(payload.name);
       this.activateNextGame(payload.name);
