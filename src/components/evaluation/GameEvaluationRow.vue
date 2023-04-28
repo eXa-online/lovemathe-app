9991580d9320afb164428a25b84b5754e8d91489
Daniel Weiß <dweiss@exa-online.de>
2023-04-28 10:20:24 +0200

    LM-141 Combine quantity tests to improve evaluation

---
+++ src/components/evaluation/GameEvaluationRow.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="gameItem">
+    <span v-if="game.level >=4" class="gameItemCell">
+      <img class="markIcon" :src="checkmark" />
+    </span>
+    <span v-else class="gameItemCell">
+      <img class="markIcon" :src="crossmark" />
+    </span>
+    <span class="gameItemCell gameItemCell__name">{{ game.verboseName }}</span>
+    <strong class="gameItemCell points">{{ game.level }}/{{ game.maxLevel }}</strong>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GameEvaluationRow",
+  props: {
+    game: {
+      type: Object,
+      required: true,
+    }
+  },
+  data() {
+    return {
+      checkmark: require("../../assets/games_evaluation/checkmark.svg"),
+      crossmark: require("../../assets/games_evaluation/crossmark.svg"),
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+.gameItem {
+  display: flex;
+  flex-direction: row;
+}
+.gameItemCell {
+  margin: 10px;
+}
+.points {
+  min-width: 50px;
+  text-align: center;
+}
+.gameItemCell__name {
+  min-width: 15vw;
+  flex-grow: 1;
+  text-align: left;
+}
+.markIcon {
+  height: 16px;
+  width: 16px;
+}
+</style>
