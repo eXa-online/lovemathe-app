ef9ff880b3875b3a9818a3ff02b54d5479c6a482
Daniel Weiß <dweiss@exa-online.de>
2023-11-02 10:39:13 +0100

    LM-171 Change last puzzle image and solution for `add_quantities`

--- src/components/AddQuantities.vue
+++ src/components/AddQuantities.vue
@@ -4,7 +4,7 @@
     :titles="['Ich brauche gleich viele Marmeladengläser wie Körbe. Wie viele Marmeladengläser fehlen mir noch?']"
     :countButtons="3"
     :showDemo="true"
-    :solutions="[0,2,0,1,0,2,1,0]"
+    :solutions="[0,2,0,1,0,2,1,2]"
     :audioDuration="4000"/>
 </template>
 
