89dd6ce661b4541f5e50b82aee621a6e7a7a3338
Daniel Weiß <dweiss@exa-online.de>
2023-11-02 09:14:53 +0100

    LM-180, LM-181 Update `complete_seriation` game with new puzzles and logic

--- src/components/CompleteSeriation.vue
+++ src/components/CompleteSeriation.vue
@@ -2,9 +2,18 @@
   <GameTemplate
     gameName="Complete_Seriation"
     :titles="['Schau dir die Punkte auf den Eimern genau an! Welcher Eimer gehört in die Lücke? Klicke auf Ihn.']"
-    :countButtons="3"
+    :buttons="[
+      [1,2,4],
+      [1,2,4],
+      [1,2,4],
+      [0,1,2,3],
+      [1,2,4],
+      [1,2,4],
+      [1,2,4],
+      [0,1,2,3],
+    ]"
     :showDemo="true"
-    :solutions="[1,2,0,1,2,0]"
+    :solutions="[1,2,0,3,1,2,0,3]"
     :audioDuration="3000"/>
 </template>
 
